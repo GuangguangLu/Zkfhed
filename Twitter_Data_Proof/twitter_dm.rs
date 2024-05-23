5ffe08f4de926a8d7e6ef1815fa78dc11e8ffd65
Guangguang Lu <97269757+GuangguangLu@users.noreply.github.com>
2024-05-23 08:32:10 +0800

    Add files via upload

---
+++ Twitter_Data_Proof/twitter_dm.rs
@@ -0,0 +1,197 @@
+// This example shows how to notarize Twitter DMs.
+//
+// The example uses the notary server implemented in ../../../notary-server
+
+
+use http_body_util::{BodyExt, Empty};
+use hyper::{body::Bytes, Request, StatusCode};
+use hyper_util::rt::TokioIo;
+use std::{env, str};
+use tlsn_core::{commitment::CommitmentKind, proof::TlsProof};
+use tlsn_examples::request_notarization;
+use tokio::io::AsyncWriteExt as _;
+use tokio_util::compat::{FuturesAsyncReadCompatExt, TokioAsyncReadCompatExt};
+use tracing::debug;
+
+use tlsn_prover::tls::{Prover, ProverConfig};
+
+// Setting of the application server
+const SERVER_DOMAIN: &str = "twitter.com";
+const ROUTE: &str = "i/api/1.1/dm/conversation";
+const USER_AGENT: &str = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36";
+
+// Setting of the notary server — make sure these are the same with those in ../../../notary-server
+const NOTARY_HOST: &str = "127.0.0.1";
+const NOTARY_PORT: u16 = 7047;
+
+// Configuration of notarization  
+const NOTARY_MAX_TRANSCRIPT_SIZE: usize = 16384;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();   
+
+    // Load secret variables frome environment for twitter server connection
+    dotenv::dotenv().ok();   
+    let conversation_id = env::var("CONVERSATION_ID").unwrap();
+    let auth_token = env::var("AUTH_TOKEN").unwrap();
+    let access_token = env::var("ACCESS_TOKEN").unwrap();
+    let csrf_token = env::var("CSRF_TOKEN").unwrap();
+
+    let (notary_tls_socket, session_id) =
+        request_notarization(NOTARY_HOST, NOTARY_PORT, Some(NOTARY_MAX_TRANSCRIPT_SIZE)).await;  
+
+    // Basic default prover config using the session_id returned from /session endpoint just now
+    // ===
+    let config = ProverConfig::builder()    
+        .id(session_id)
+        .server_dns(SERVER_DOMAIN)
+        .build()
+        .unwrap();
+
+    // Create a new prover and set up the MPC backend. 
+    let prover = Prover::new(config)
+        .setup(notary_tls_socket.compat())
+        .await
+        .unwrap();
+
+        // ===  
+    let client_socket = tokio::net::TcpStream::connect((SERVER_DOMAIN, 443))
+        .await
+        .unwrap();
+
+    // Bind the Prover to server connection
+    let (tls_connection, prover_fut) = prover.connect(client_socket.compat()).await.unwrap();  
+    let tls_connection = TokioIo::new(tls_connection.compat());   
+
+    // Grab a control handle to the Prover
+    let prover_ctrl = prover_fut.control();   
+
+    // Spawn the Prover to be run concurrently
+    let prover_task = tokio::spawn(prover_fut);  
+
+    // Attach the hyper HTTP client to the TLS connection
+    let (mut request_sender, connection) = hyper::client::conn::http1::handshake(tls_connection)
+        .await 
+        .unwrap();  
+
+    // Spawn the HTTP task to be run concurrently
+    tokio::spawn(connection);   
+
+    // Build the HTTP request to fetch the DMs  ，  构造请求头。
+    let request = Request::builder()    
+        .uri(format!(
+            "https://{SERVER_DOMAIN}/{ROUTE}/{conversation_id}.json" 
+        ))
+        .header("Host", SERVER_DOMAIN)
+        .header("Accept", "*/*")
+        .header("Accept-Encoding", "identity")
+        .header("Connection", "close")
+        .header("User-Agent", USER_AGENT)
+        .header("Authorization", format!("Bearer {access_token}"))
+        .header(
+            "Cookie",
+            format!("auth_token={auth_token}; ct0={csrf_token}"),
+        )
+        .header("Authority", SERVER_DOMAIN)
+        .header("X-Twitter-Auth-Type", "OAuth2Session")
+        .header("x-twitter-active-user", "yes")
+        .header("X-Csrf-Token", csrf_token.clone())
+        .body(Empty::<Bytes>::new())
+        .unwrap();
+
+    debug!("Sending request");
+
+    // Because we don't need to decrypt the response right away, we can defer decryption
+    // until after the connection is closed. This will speed up the proving process!
+    prover_ctrl.defer_decryption().await.unwrap();   
+
+    let response = request_sender.send_request(request).await.unwrap();   //接收到response
+
+    debug!("Sent request");
+
+    assert!(response.status() == StatusCode::OK, "{}", response.status());
+
+    debug!("Request OK");
+
+    // Pretty printing :)
+    let payload = response.into_body().collect().await.unwrap().to_bytes();
+    let parsed =
+        serde_json::from_str::<serde_json::Value>(&String::from_utf8_lossy(&payload)).unwrap();   // ===
+    debug!("{}", serde_json::to_string_pretty(&parsed).unwrap());  
+
+    // The Prover task should be done now, so we can grab it.
+    let prover = prover_task.await.unwrap().unwrap(); 
+
+    // Upgrade the prover to an HTTP prover, and start notarization.   将证明程序升级为HTTP证明程序，然后开始公证。
+    let mut prover = prover.to_http().unwrap().start_notarize();      
+
+    // Commit to the transcript with the default committer, which will commit using BLAKE3.
+    prover.commit().unwrap();  
+
+    // Finalize, returning the notarized HTTP session
+    let notarized_session = prover.finalize().await.unwrap();   
+
+    debug!("Notarization complete!");
+
+    // Dump the notarized session to a file  将公证会话数据保存到文件的代码块。
+    let mut file = tokio::fs::File::create("twitter_dm.json").await.unwrap();
+    file.write_all(
+        serde_json::to_string_pretty(notarized_session.session())
+            .unwrap()
+            .as_bytes(),
+    )
+    .await
+    .unwrap();
+
+    let session_proof = notarized_session.session_proof();  
+
+    let mut proof_builder = notarized_session.session().data().build_substrings_proof();  //构造证明
+
+    // Prove the request, while redacting the secrets from it.   证明请求，同时编辑其中的秘密。
+    let request = &notarized_session.transcript().requests[0];
+
+    proof_builder
+        .reveal_sent(&request.without_data(), CommitmentKind::Blake3)
+        .unwrap();
+
+    proof_builder
+        .reveal_sent(&request.request.target, CommitmentKind::Blake3)
+        .unwrap();
+
+    for header in &request.headers {
+        // Only reveal the host header   reveal：泄露
+        if header.name.as_str().eq_ignore_ascii_case("Host") {
+            proof_builder
+                .reveal_sent(header, CommitmentKind::Blake3)  //泄露
+                .unwrap();
+        } else {
+            proof_builder
+                .reveal_sent(&header.without_value(), CommitmentKind::Blake3)  //without_value() 不泄露
+                .unwrap();
+        }
+    }
+
+    // Prove the entire response, as we don't need to redact anything   证明整个回应，不需要编辑任何内容
+    let response = &notarized_session.transcript().responses[0];
+
+    proof_builder
+        .reveal_recv(response, CommitmentKind::Blake3)  
+        .unwrap();
+
+    // Build the proof
+    let substrings_proof = proof_builder.build().unwrap();
+
+    let proof = TlsProof {
+        session: session_proof,
+        substrings: substrings_proof,
+    };
+
+    // Dump the proof to a file.
+    let mut file = tokio::fs::File::create("twitter_dm_proof.json")
+        .await
+        .unwrap();
+    file.write_all(serde_json::to_string_pretty(&proof).unwrap().as_bytes())
+        .await
+        .unwrap();
+}
