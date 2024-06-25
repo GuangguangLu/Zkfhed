29ec148de0e44e1c4c32044a4967034630ed1ca7
Guangguang Lu <97269757+GuangguangLu@users.noreply.github.com>
2024-06-25 18:03:31 +0800

    Update PowerManagement.sol

--- contracts/PowerManagement.sol
+++ contracts/PowerManagement.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.0;
 
 contract PowerManagement {
 
-    // 定义事件来记录worker的注册
+
     event WorkerRegistered(address indexed workerAddress, string machineResources);
 
-    // 注册Worker
+
     function registerWorker(string memory _machineResources) public {
-        // 触发事件，记录worker的注册信息
+
         emit WorkerRegistered(msg.sender, _machineResources);
     }
 }
